5fec4c149adf4dfe240afa46b1a115df950703fd
Rogerio Campos <rogerio.campos07@gmail.com>
2024-09-15 18:40:24 -0300

    add:functions argparse

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,5 +1,48 @@
+import argparse
+
+def load(filepath):
+    """Load data from filepath to database"""
+    try:
+        with open(filepath) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError as e:
+        print(f'File not found {e}')
+
+
+
 
 def main():
-    print("Executing dundie from entry point")
+    parser = argparse.ArgumentParser(
+        description="Dunder Mifflin Rewards",
+        epilog="Enjoy and use with cautions"
+    )
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="The subcommand to run",
+        choices=("load","show","send"),
+        default="help"
+
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="filepath to load",
+        default=None
+
+    )
+
+    args = parser.parse_args()
+
+    globals()[args.subcommand](args.filepath)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
 
 
