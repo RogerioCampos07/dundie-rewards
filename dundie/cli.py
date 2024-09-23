d7ff97f4739762159590a78173888abbfb4bf472
Rogerio Campos <rogerio.campos07@gmail.com>
2024-09-23 02:20:28 -0300

    separação das funções nos moulos e deixando o __main__.py somente como entrypoint

---
+++ dundie/cli.py
@@ -0,0 +1,27 @@
+import argparse
+from dundie.core import load
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Dunder Mifflin Rewards",
+        epilog="Enjoy and use with cautions"
+    )
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="The subcommand to run",
+        choices=("load","show","send"),
+        default="help"
+
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="filepath to load",
+        default=None
+
+    )
+
+    args = parser.parse_args()
+
+    globals()[args.subcommand](args.filepath)
---
+++ dundie/core.py
@@ -0,0 +1,13 @@
+'''Core module of dundie'''
+from dundie.utils.log import get_logger
+
+log = get_logger()
+
+def load(filepath):
+    """Load data from filepath to database"""
+    try:
+        with open(filepath) as file_:
+            return file_.readlines()
+    except FileNotFoundError as e:
+        log.error(str(e))
+        raise e
