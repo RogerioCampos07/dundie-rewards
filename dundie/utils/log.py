197cc8a63043771c2ee0ac027279ebe5f8c94466
Rogerio Campos <rogerio.campos07@gmail.com>
2024-09-23 02:21:16 -0300

    add: Logging ao projeto

---
+++ dundie/utils/log.py
@@ -0,0 +1,25 @@
+import os
+import logging
+from logging import handlers
+
+LOG_LEVEL= os.getenv("LOG_LEVEL","WARNING").upper()
+log = logging.getLogger("dundie")
+fmt = logging.Formatter(
+        '%(asctime)s %(name)s %(levelname)s'
+        '1:(lineno)d f:%(filename)s: %(message)s'
+    )
+
+def get_logger(logfile="dundie.log"):
+    fh = handlers.RotatingFileHandler(
+        logfile,
+        maxBytes=300,
+        backupCount=10,
+    )
+    fh.setlevel(LOG_LEVEL)
+    fh.setFormatter(fmt)
+    log.addHandler(fh)
+    return log
+    
+
+    
+    
